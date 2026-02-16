f531ff9a0597b9e40fde720264ef3a64cdd24760
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2026-02-16 10:34:27 -0300

    tela inicial e de creditos pq sim

---
+++ felpojams-dos-exlabers/Codigos/tela_creditos.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@onready var scroll_container: ScrollContainer = $ScrollContainer
+
+@export var text_node: RichTextLabel
+@export var velo : float = 1
+var acabou : bool
+
+func _ready() -> void:
+	acabou = false
+	
+func fim():
+	acabou = true
+	print("fim")
+	
+	
+func _process(delta: float) -> void:
+	if scroll_container.scroll_vertical <= text_node.size.y+680:
+		scroll_container.scroll_vertical += 1 * velo
+	elif !acabou:
+		fim()
