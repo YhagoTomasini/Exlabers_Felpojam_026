dc0ef83a257f94f743d7f67c81d50ab94c3595f6
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2026-02-17 20:22:15 -0300

    update range carimbo

--- felpojams-dos-exlabers/Codigos/carimbo.gd
+++ felpojams-dos-exlabers/Codigos/carimbo.gd
@@ -11,13 +11,37 @@ extends Node2D
 
 #Precisa pegar a referencia da UI para ativar uma função no código dela
 @onready var uiTntas = $"../UI_Tintas"
+@onready var player = $"../PersonagemPrincipal"
+
+#marca hipotetica
+@onready var marcaHipoteticaCena = preload("res://Prefabs/marcaHipotetica.tscn")
+var marcaHipotetica
+var rangeMH = 120
 
 func _ready() -> void:
 	pass
 	
 func _process(delta: float) -> void:
-	#Clica
-	if Input.is_action_just_pressed("click"):
+	#Vou mudar esse código:
+	#Enquanto estiver segurando o "clickRight"
+	#intantiate "marcaHipotetica" a X pixels do player na direção da posição local do mouse
+	#Se apertar "clickLeft" instancia a "marca" na posição em que estava a "marcaHipotetica"
+	#something like:
+	
+	if Input.is_action_pressed("clickRight"):
+		if marcaHipotetica == null:
+			marcaHipotetica = marcaHipoteticaCena.instantiate()
+			add_child(marcaHipotetica)
+		
+		var direção = (get_local_mouse_position() - player.position).normalized()
+		marcaHipotetica.position = player.position + direção * rangeMH
+		
+	else:
+		if marcaHipotetica != null:
+			marcaHipotetica.queue_free()
+			marcaHipotetica = null
+		
+	if Input.is_action_just_pressed("clickLeft") and marcaHipotetica != null:
 		#Se existir a UI pede para ativar a função atualizar_barra nela
 		if uiTntas != null:
 			uiTntas.atualizar_barra()
@@ -37,5 +61,5 @@ func _process(delta: float) -> void:
 		elif Globals.corAtual == 3:
 			marcaInstanciada = cenaMarcaC.instantiate()
 		
-		marcaInstanciada.global_position = get_local_mouse_position() #instancia na posição do mouse
+		marcaInstanciada.global_position = marcaHipotetica.position
 		add_child(marcaInstanciada)
