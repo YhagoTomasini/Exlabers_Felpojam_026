51448890fc2a8ef09f4f8197d147473c616a1533
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2026-02-17 23:42:31 -0300

    atualização menu

--- felpojams-dos-exlabers/Codigos/tela_inicial.gd
+++ felpojams-dos-exlabers/Codigos/tela_inicial.gd
@@ -8,3 +8,7 @@ func _on_button_button_down() -> void:
 
 func _on_button_2_button_down() -> void:
 	get_tree().change_scene_to_file("res://Cenas/tela_creditos.tscn")
+
+
+func _on_button_3_button_down() -> void:
+	get_tree().quit()
