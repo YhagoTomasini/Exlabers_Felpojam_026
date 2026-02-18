8451a4b34aea8844b9af87caedc33322c688c127
Thaymoz <artemiorabello123@gmail.com>
2026-02-17 22:56:35 -0300

    Pulo matematicamente correto

--- felpojams-dos-exlabers/Codigos/personagem_principal.gd
+++ felpojams-dos-exlabers/Codigos/personagem_principal.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const MAGENTA_FORCE = -400.0
+const AIR_FRICTION := 0.7
 
 #temos uma booleana para verificação se esta na marca ciano,
 #e uma inteira para verificar em quantos ciano o personagem esta
@@ -12,24 +13,43 @@ var noCianoI : int
 var knockback_vector := Vector2.ZERO
 var knockback_power := 20 
 
+var is_jumping := false
+@export var jump_heigh := 128
+@export var max_time_to_peak := 0.5
+var jump_velocity
+var gravity
+var fall_gravity
+
+
 func _ready() -> void:
 	noCianoB = false
 	noCianoI = 0
+	jump_velocity= (jump_heigh*2)/ max_time_to_peak
+	gravity= (jump_heigh*2)/ pow(max_time_to_peak,2)
+	fall_gravity = gravity*2
 
 func _physics_process(delta: float) -> void:
 	#Se n estiver no chão e n estiver no ciano cai normal,
 	if not is_on_floor() and !noCianoB:
-		velocity += get_gravity() * delta
+		velocity.x = 0
 	#Se n estiver no chão e estiver no ciano cai devagar
 	elif not is_on_floor() and noCianoB:
 		velocity += (get_gravity() * delta)/4
 
 	#Se estiver no chão e não estiver em um ciano pula normal
 	if Input.is_action_just_pressed("ui_up") and is_on_floor() and !noCianoB:
-		velocity.y = JUMP_VELOCITY
+		velocity.y = -jump_velocity
+		is_jumping = true
 	#Se estiver no ciano pode precionar que você sobe devagar... até sair do ciano
 	elif Input.is_action_pressed("ui_up") and noCianoB:
-		velocity.y = JUMP_VELOCITY/4
+		velocity.y = -jump_velocity/4
+	elif is_on_floor():
+		is_jumping = false
+		
+	if velocity.y > 0 or not Input.is_action_pressed("ui_up"):
+		velocity.y += fall_gravity*delta
+	else:
+		velocity.y += gravity*delta
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -41,7 +61,6 @@ func _physics_process(delta: float) -> void:
 	if knockback_vector != Vector2.ZERO:
 		velocity = knockback_vector
 	move_and_slide()
-
 
 func _on_area_2d_personagem_area_entered(area: Area2D) -> void:
 	#Passar de Lvl e recebe uma nova cor de tinta
@@ -67,7 +86,7 @@ func _on_area_2d_personagem_area_entered(area: Area2D) -> void:
 		#var knockback = Vector2((global_position.x - area.global_position.x) * knockback_power, -200)
 		#empurra(knockback)
 		#Só comentar e descomentar se quiser voltar o knocback
-		velocity.y = JUMP_VELOCITY*1.7
+		velocity.y = MAGENTA_FORCE*1.7
 
 func _on_area_2d_personagem_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	if area != null:
