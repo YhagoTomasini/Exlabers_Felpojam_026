697f1584e49efdff99cc8d9150a4871144753278
Thaymoz <artemiorabello123@gmail.com>
2026-02-18 15:38:25 -0300

    Disparador de tiros + tiro

---
+++ felpojams-dos-exlabers/Codigos/dipsarador.gd
@@ -0,0 +1,40 @@
+extends StaticBody2D
+
+const TIRO = preload("uid://dlajl544ftnv")
+
+@onready var marker: Marker2D = $marker
+@onready var cd_tiro: Timer = $cd_tiro
+@onready var detector: RayCast2D = $detector
+
+
+@export var para_direita : bool = true
+
+
+func _ready() -> void:
+	if !para_direita:
+		scale.x *= -1
+
+
+func _process(_delta: float) -> void:
+	if detector.is_colliding():
+		if cd_tiro.is_stopped():
+			cd_tiro.start()
+#Se quiser que ele atire na hora q entrar na area
+			#spawn_tiro()
+	else:
+		cd_tiro.stop()
+
+
+func spawn_tiro():
+	var novo_tiro = TIRO.instantiate()
+	if para_direita:
+		novo_tiro.qual_direcao(1)
+	else:
+		novo_tiro.qual_direcao(-1)
+	add_sibling(novo_tiro)
+	novo_tiro.global_position = marker.global_position
+
+
+func _on_cd_tiro_timeout() -> void:
+	spawn_tiro()
+	print("atirou")
---
+++ felpojams-dos-exlabers/Codigos/tiro.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@onready var anim: AnimatedSprite2D = $anim
+
+var velocidade_do_tiro := 360
+var direcao := 1 
+
+func _process(delta: float) -> void:
+	position.x += velocidade_do_tiro * direcao * delta
+	
+
+func qual_direcao(dir):
+	direcao = dir
+	if dir < 0:
+		$anim.flip_h = true
+	else:
+		$anim.flip_h = false
+
+
+func _on_player_detector_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		queue_free()
+		#body.player_morreu()
