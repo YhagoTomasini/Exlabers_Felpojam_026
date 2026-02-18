efa784e88f2497f2afc86917f14510c3dc4b6102
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2026-02-18 07:59:57 -0300

    atualização botão e porta

--- felpojams-dos-exlabers/Codigos/pressure_plate.gd
+++ felpojams-dos-exlabers/Codigos/pressure_plate.gd
@@ -1,20 +1,42 @@
 extends Area2D
 var pressionado : bool = false
 @export var alvo_porta : Node2D 
+@export var sprite : AnimatedSprite2D
 
 func _ready() -> void:
 	print(pressionado)
 
 
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "body_marcaY":
-		pressionado = true
-		desativar_colisao_porta(true)
-
 func desativar_colisao_porta(valor: bool):
 	if alvo_porta:
 		# Usamos o find_child para garantir que pegamos o nó de colisão correto
 		var colisao = alvo_porta.find_child("collision")
-		if colisao:
-			# Usar set_deferred é obrigatório se a colisão mudar durante um impacto
-			colisao.set_deferred("disabled", valor)
+		var portaAnim : AnimatedSprite2D = alvo_porta.find_child("sprite")
+		
+		if valor:
+			if portaAnim:
+				portaAnim.play("aberta")
+			if colisao:
+				# Usar set_deferred é obrigatório se a colisão mudar durante um impacto
+				colisao.set_deferred("disabled", valor)
+				
+		else:
+			if portaAnim:
+				portaAnim.play("default")
+			if colisao:
+				# Usar set_deferred é obrigatório se a colisão mudar durante um impacto
+				colisao.set_deferred("disabled", valor)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	#if body.name == "body_marcaY":
+	pressionado = true
+	sprite.play("apertado")
+	
+	desativar_colisao_porta(true)
+
+func _on_body_exited(body: Node2D) -> void:
+	pressionado = false
+	sprite.play("default")
+	
+	desativar_colisao_porta(false)
---
+++ felpojams-dos-exlabers/Prefabs/porta.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+@export var sprite : AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	sprite.play("default")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
