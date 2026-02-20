de8adb6d5919ccc8a8b4b10ab3100d3307be773e
Tamires <tamires.ei04@gmail.com>
2026-02-20 00:52:10 -0300

    menu com parallax

--- felpojams-dos-exlabers/Codigos/tela_inicial.gd
+++ felpojams-dos-exlabers/Codigos/tela_inicial.gd
@@ -1,6 +1,19 @@
 extends Control
 
-@onready var jogarB : Button = $VBoxContainer/Button
+@onready var jogarB : Button = $CanvasLayer/VBoxContainer/Button
+@onready var parallax = $ParallaxBG
+
+var intensidade = 30.0
+var suavidade = 5.0
+var alvo = Vector2.ZERO
+
+func _process(delta):
+	var mouse_pos = get_viewport().get_mouse_position()
+	var screen_size = get_viewport_rect().size
+	
+	var offset = (mouse_pos - screen_size / 2.0) / (screen_size / 2.0)
+	alvo = offset * intensidade
+	parallax.scroll_offset = parallax.scroll_offset.lerp(alvo, delta * suavidade)
 
 func _ready() -> void:
 	jogarB.grab_focus()
