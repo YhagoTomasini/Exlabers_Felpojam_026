287b5bdb0085235a2ab643fecac84053a235b7f1
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2026-02-23 12:56:05 -0300

    a mecanica funciona

--- felpojams-dos-exlabers/Cenas/Prefabs_gerais/personagem_principal.gd
+++ felpojams-dos-exlabers/Cenas/Prefabs_gerais/personagem_principal.gd
@@ -98,14 +98,17 @@ func _on_area_2d_personagem_area_entered(area: Area2D) -> void:
 	#Passar de Lvl e recebe uma nova cor de tinta
 	if area.name == "FinalLvl1":
 		Globals.arrayTintas.append(Globals.amarelo)
+		Globals.saveTanqueCena()
 		get_tree().change_scene_to_file("res://Cenas/lvl_2.tscn")
 		#area.queue_free()
 	elif area.name == "FinalLvl2":
 		Globals.arrayTintas.append(Globals.magenta)
+		Globals.saveTanqueCena()
 		get_tree().change_scene_to_file("res://Cenas/lvl_3.tscn")
 		#area.queue_free()
 	elif area.name == "FinalLvl3":
 		Globals.arrayTintas.append(Globals.ciano)
+		Globals.saveTanqueCena()
 		get_tree().change_scene_to_file("res://Cenas/lvl_4.tscn")
 		#area.queue_free()
 	elif area.name == "FinalLvl4":
