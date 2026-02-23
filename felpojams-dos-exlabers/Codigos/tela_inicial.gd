a8e2b0c7e2a8454cecf96c6a1841f03d21d0cbee
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2026-02-23 07:35:17 -0300

    ajustes no direcional das cenas

--- felpojams-dos-exlabers/Codigos/tela_inicial.gd
+++ felpojams-dos-exlabers/Codigos/tela_inicial.gd
@@ -44,7 +44,7 @@ func _process(delta):
 	parallax.scroll_offset = parallax.scroll_offset.lerp(alvo, delta * suavidade)
 
 func _on_jogar_pressed() -> void:
-	get_tree().change_scene_to_file("res://Cenas/sala_de_testes.tscn")
+	get_tree().change_scene_to_file("res://Cenas/lvl_1.tscn")
 	#button_sound.play()
 
 func _on_creditos_pressed() -> void:
