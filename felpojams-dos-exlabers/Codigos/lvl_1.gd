f3ac069f7f3e4cc442351fae334e5370bc3eb3e3
Tamires <tamires.ei04@gmail.com>
2026-02-25 14:48:22 -0300

    transição no level 1

--- felpojams-dos-exlabers/Codigos/lvl_1.gd
+++ felpojams-dos-exlabers/Codigos/lvl_1.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+@onready var transicao: CanvasLayer = $CanvasLayer/Transição
+@onready var animacao = $CanvasLayer/Transição/ColorRect/Animation
+
+@export var anim_duracao : float = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	AudioManager.criar_aud(SoundEffect.TIPO_DE_SOM.TEMA2)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	animacao.speed_scale = anim_duracao
+	animacao.play("transição_out")
+	await animacao.animation_finished
+	transicao.visible = false
