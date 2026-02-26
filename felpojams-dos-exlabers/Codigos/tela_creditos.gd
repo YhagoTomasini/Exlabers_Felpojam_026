6e9765e3b6a3c3290eac4d716039710051a60003
Tamires <tamires.ei04@gmail.com>
2026-02-26 11:36:06 -0300

    arrumando os creditos

--- felpojams-dos-exlabers/Codigos/tela_creditos.gd
+++ felpojams-dos-exlabers/Codigos/tela_creditos.gd
@@ -1,33 +1,48 @@
 extends Control
 
-#Literalmente o mesmo código que usei nos créditos de itsy bitsy spider e crysantiun
-
 @export var scroll_container: ScrollContainer
-
+@export var fundo: TextureRect
 @export var text_node: RichTextLabel
-@export var velo : float = 1
-var acabou : bool
+@export var velo: float = 1.0
+@export var pause: Control
 
-@export var pause : Control
+var acabou: bool = false
+var scroll_shader: ShaderMaterial
+
 
 func _ready() -> void:
 	acabou = false
 	
+	# Pega o material do fundo
+	scroll_shader = fundo.material as ShaderMaterial
+
+
 func fim():
 	acabou = true
 	pause.pausar()
 	print("fim")
-	
+
+
 func _process(delta: float) -> void:
-	if !acabou:
-		if scroll_container.scroll_vertical <= text_node.size.y+100:
-			scroll_container.scroll_vertical += 1 * velo
-			#print(scroll_container.scroll_vertical)
-		else:
-			#print("sssss")
-			fim()
+	if acabou:
+		return
+	
+	# Controle de velocidade
+	if Input.is_action_pressed("ui_up"):
+		velo = 6.0
+	else:
+		velo = 1.0
+	
+	# Continua rolando
+	if scroll_container.scroll_vertical <= text_node.size.y + 100:
 		
-		if Input.is_action_pressed("ui_up"):
-			velo = 6.0
-		else:
-			velo = 1.0
+		scroll_container.scroll_vertical += velo
+		
+		# Move o fundo via shader (valor pequeno para não ficar rápido demais)
+		scroll_shader.set_shader_parameter(
+			"scroll_y",
+			scroll_container.scroll_vertical / 1500.0
+		)
+		
+	else:
+		fim()
