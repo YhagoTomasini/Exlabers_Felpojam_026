0edb1268dfe1636af9275082498ec0f5d01d68c1
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2026-03-14 12:54:59 -0300

    fundo

--- felpojams-dos-exlabers/Codigos/tela_scores.gd
+++ felpojams-dos-exlabers/Codigos/tela_scores.gd
@@ -4,6 +4,16 @@ extends Control
 @export var score : Label
 @export var hScore : Label
 
+@export var parallax : Parallax2D
+
+@export var botao_jogar : Button
+
+var intensidade = 30.0
+var suavidade = 5.0
+var alvo = Vector2.ZERO
+
+var usando_teclado = false
+
 func _ready() -> void:
 	ContadorTempo.parar_timer()
 	
@@ -24,6 +34,38 @@ func _ready() -> void:
 	var hm = int(Globals.high_tempo / 60.0)
 	var hs = Globals.high_tempo % 60
 	hScore.text = '%02d:%02d ' % [hm, hs]
+
+func _input(event):
+	# 🎮 detecta teclado / controle
+	if event.is_action_pressed("ui_up") \
+	or event.is_action_pressed("ui_down") \
+	or event.is_action_pressed("ui_left") \
+	or event.is_action_pressed("ui_right"):
+
+		if not usando_teclado:
+			usando_teclado = true
+
+			# se ninguém estiver focado, foca o primeiro botão
+		if not get_viewport().gui_get_focus_owner():
+			botao_jogar.grab_focus()
+
+
+	# 🖱 detecta movimento do mouse
+	if event is InputEventMouseMotion:
+		if usando_teclado:
+			usando_teclado = false
+
+			var foco = get_viewport().gui_get_focus_owner()
+			if foco:
+				foco.release_focus()
+
+func _process(delta):
+	var mouse_pos = get_viewport().get_mouse_position()
+	var screen_size = get_viewport_rect().size
+	
+	var offset = (mouse_pos - screen_size / 2.0) / (screen_size / 2.0)
+	alvo = offset * intensidade
+	parallax.scroll_offset = parallax.scroll_offset.lerp(alvo, delta * suavidade)
 
 func _on_jogar_pressed() -> void:
 	ContadorTempo.reset_timer()
